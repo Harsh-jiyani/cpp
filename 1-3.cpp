6b8f029f480f5af9c677a5ceed6a570b913a7152
Harsh-jiyani <harshjiyani4277@gmail.com>
2025-01-06 11:09:07 +0530

    user input

---
+++ 1-3.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+using namespace std;
+
+int main()
+{
+int int1, int2;
+cout<<   "entr the value of num1\n" ;
+cin>>int1;
+
+cout<<   "entr the value of num2\n" ;
+cin>>int2;
+
+cout<< "the sum is\n"<< int1+int2; 
+    return 0;
+}
