0b8e9b198a80e12cc08c6cd2283d14ccae8c5cc0
Harsh-jiyani <harshjiyani4277@gmail.com>
2025-01-12 13:51:06 +0530

    Add files via upload

---
+++ 1-5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+   // int a =34;
+  /*  cout<<"the value of was:"<<a;
+    a =45;
+    cout <<"value is a is:"<<a;*/
+// ************** manipulater in c ****************
+    /*int a=12 ,b=23 , c=45;
+ cout<<"the value of is :"<<setw(4)<<a<<endl;
+ cout<<"the value of b is :"<<setw(4)<<b<<endl;
+cout<<"the value of c is :"<<setw(4)<<c<<endl;*/
+
+
+int a =3,b=4;
+int c =(a*5)+b;
+cout <<c;
+
+    
+    
+    
+    
+    
+    return 0;
+}
