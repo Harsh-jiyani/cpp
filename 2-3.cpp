edaf4aef3fe435930a099d779364826496c96632
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-01-19 11:59:20 +0530

    union

---
+++ 2-3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+struct employee
+{
+    int ID;
+    char favchar;
+    float salary;
+
+};
+union money
+{
+    int rice;
+    char car;
+    float pounds;
+};
+
+
+
+
+int main(){
+   /* struct employee harsh;
+    {
+        harsh.ID = 1;
+        harsh.favchar = 'r';
+        harsh.salary =1000000;
+
+        cout<<harsh.ID<<endl;
+        cout<<harsh.favchar<<endl;
+    }
+    
+*/
+/*union money m1;
+m1.rice =2;
+cout<<m1.rice;*/
+
+
+}
